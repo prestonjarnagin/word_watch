d617dbc0751183f9fabb2ac8a6f48224604f24fc
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-05 10:35:35 -0700

    Build working GET request for top word and display on page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  let break_down = document.getElementById('breakdown-button')
+  break_down.addEventListener('click', breakDown, false)
 })
+
+document.addEventListener("DOMContentLoaded", getTopWord);
+
+
+function breakDown(){
+
+
+}
+
+function getTopWord(){
+  fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word/`, {
+    method: 'GET',
+    headers:{'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            }
+  })
+  .then((respone) => respone.json())
+  .then((data) => {
+    let topWord = Object.keys(data.word)[0];
+    let wordCount = data.word[topWord];
+    $('#top-word-display').append(topWord);
+    $('#top-word-count-display').append(wordCount);
+  })
+
+}
